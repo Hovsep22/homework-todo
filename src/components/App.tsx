34364e301fe750e6a59780a0301bb8f2fd3a89c7
Voskan Voskanyan <voskanvoskanyan@yandex.com>
2024-05-16 19:08:35 +0400

    test

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import AddTodo from "./AddTodo";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <TodoList />
+
+      <AddTodo />
+    </div>
+  );
+}
+
+export default App;
